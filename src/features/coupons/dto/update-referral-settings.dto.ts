fcfec1c6b3cb1876b69954d538aa7846cb7225ae
Faseeh100 <syedfaseeh40@gmail.com>
2026-06-15 14:04:00 +0500

    fixing ref

--- src/features/coupons/dto/update-referral-settings.dto.ts
+++ src/features/coupons/dto/update-referral-settings.dto.ts
@@ -1,32 +1,3 @@
-// import { IsNumber, Min, Max, IsOptional, IsIn, IsString } from 'class-validator';
-
-// export class UpdateReferralSettingsDto {
-//   @IsNumber()
-//   @Min(0)
-//   @Max(1000)
-//   referrerReward: number;
-
-//   @IsNumber()
-//   @Min(0)
-//   receiverDiscount: number;
-
-//   @IsString()
-//   @IsIn(['percentage', 'fixed'])
-//   receiverDiscountType: 'percentage' | 'fixed';
-
-//   @IsNumber()
-//   @IsOptional()
-//   @Min(0)
-//   minOrderAmount?: number;
-
-//   @IsNumber()
-//   @IsOptional()
-//   @Min(0)
-//   maxDiscountAmount?: number;
-// }
-
-
-
 import { IsNumber, Min, Max, IsOptional, IsIn, IsString } from 'class-validator';
 
 export class UpdateReferralSettingsDto {
